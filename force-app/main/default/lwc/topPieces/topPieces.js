1479698a650537dd10b654aff31e3b10e90fcd57
Gordon Caister <gordoncaister@gmail.com>
2020-08-25 17:38:47 -0500

    copied over files

---
+++ force-app/main/default/lwc/topPieces/topPieces.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+import { LightningElement, api, track } from "lwc";
+
+import getTopPieces from "@salesforce/apex/TopPiecesController.getTopPieces";
+
+export default class TopPieces extends LightningElement {
+  @api recordId;
+  @track topPieces = [];
+
+  // On init
+  connectedCallback() {
+    this.getTopPieces();
+  }
+
+  getTopPieces() {
+    this.executeAction(getTopPieces, { vendorId: this.recordId, num: 3 })
+      .then((pieces) => {
+        this.topPieces = pieces;
+        console.log(
+          "top Pieces",
+          this.recordId,
+          JSON.parse(JSON.stringify(pieces))
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  executeAction(apexMethod, params) {
+    return new Promise(function (resolve, reject) {
+      apexMethod(params)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+}
